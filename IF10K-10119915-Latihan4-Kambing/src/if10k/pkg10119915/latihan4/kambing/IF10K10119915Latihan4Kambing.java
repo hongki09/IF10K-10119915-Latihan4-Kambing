eb0bfd815774ebca5611920fe1a1f96813aa84a8
hongki09 <73209580+hongki09@users.noreply.github.com>
2020-10-21 10:26:32 +0700

    Add files via upload
    first commit

---
+++ IF10K-10119915-Latihan4-Kambing/src/if10k/pkg10119915/latihan4/kambing/IF10K10119915Latihan4Kambing.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119915.latihan4.kambing;
+
+/**
+ *
+ * @author 
+ * Nama      : M Hongki Alfikram
+ * NIM       : 10119915
+ * Kelas     : IF-10K
+ * Deskripsi : deklarasi Variable Lokal.
+ */
+public class IF10K10119915Latihan4Kambing {
+
+    /**
+     */
+    public void tambahKambing() {
+        // TODO code application logic here
+    
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah penambahan " +jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        IF10K10119915Latihan4Kambing kambingJantan = new IF10K10119915Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }        
+}
